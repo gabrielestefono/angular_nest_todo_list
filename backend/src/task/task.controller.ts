2efd7bd1c2532bde85a8618ac376b108247f9db1
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-29 07:06:36 -0300

    Criando services

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,59 +1,26 @@
+import { TaskService } from './task.service';
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('task')
 export class TaskController {
-	tasks = [
-		{
-			id: 1,
-			nome: "Task 01",
-			concluida: false,
-			deletedAt: null
-		},
-		{
-			id: 2,
-			nome: "Task 02",
-			concluida: false,
-			deletedAt: null
-		},
-		{
-			id: 3,
-			nome: "Task 03",
-			concluida: false,
-			deletedAt: null
-		},
-		{
-			id: 4,
-			nome: "Task 04",
-			concluida: false,
-			deletedAt: null
-		}
-	]
-
+	constructor(private readonly taskService: TaskService){}
 	@Get()
 	getAll(){
-		return this.tasks;
+		return this.taskService.findAll();
 	}
 
 	@Post()
 	create(@Body() body: any){
-		this.tasks.push(body);
-		return body;
+		return this.taskService.create(body);
 	}
 
 	@Patch(':id')
 	markAsConcluded(@Param('id') id: number){
-		const task = this.tasks.find(task => task.id === +id);
-		task.concluida = true;
-		return task;
+		return this.taskService.update(+id);
 	}
 
 	@Delete(':id')
-	remove(id: number){
-		const index = this.tasks.findIndex(task => task.id === id);
-		if(index >= 0){
-			this.tasks.splice(index, 1);
-		}
-		return {mensage: "Deletado com sucesso!"}
+	remove(@Param('id') id: number){
+		this.taskService.delete(+id);
 	}
-
 }
