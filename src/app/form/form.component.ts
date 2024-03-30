32616db69ae53d2bfcaa74749a3bf7163901a2ec
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-30 13:55:41 -0300

    Finalizando Frontend

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import WebService from '../../webservice';
 
 @Component({
@@ -18,5 +18,4 @@ export class FormComponent{
       this.task = '';
     }    
   }
-
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -8,12 +8,28 @@ import WebService from '../../webservice';
 })
 export class TaskListComponent implements OnInit {
   public tasks: any;
+  public tasksCount: number = 0;
+  public tasksCountConcluded: number = 0;
 
   constructor(private webService: WebService) { }
 
   ngOnInit(): void {
-    this.webService.tasks$.subscribe(tasks => { 
+    this.webService.tasks$.subscribe(tasks => {
+      tasks.sort((a: any, b: any)=>{
+        if(a.concluida && !b.concluida){
+          return 1;
+        }else if (!a.concluida && b.concluida) {
+          return -1;
+        }else{
+          return 0;
+        }
+      })
       this.tasks = tasks;
+      this.tasksCount = tasks.length;
+      let tasksConcluded = tasks.filter((task: any)=>{
+        return task.concluida;
+      })
+      this.tasksCountConcluded = tasksConcluded.length;
     });
    
     this.webService.getTasks();
